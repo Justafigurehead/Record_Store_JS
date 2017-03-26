b5fc1a62531c8beeffcf6a9b375766ee759bb10d
Justafigurehead <strachanmichaela@hotmail.com>
2017-03-26 12:29:54 +0100

    Added pending tests and added balance to Record Store.

--- record_store.js
+++ record_store.js
@@ -1,9 +1,13 @@
 var RecordStore = function(name, city){
   this.name = name;
   this.city = city;
+  this.balance = 0;
   this.inventory = [];
   this.getInventory = function(){
     return this.inventory.length;
+  }
+  this.addInventory = function(record){
+    this.inventory.push(record);
   }
 }
 
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -20,13 +20,23 @@ describe('Record Store', function(){
     assert.equal('Glasgow', recordStore.city)
   });
 
+  it('should have a zero balance', function(){
+    assert.equal(0, recordStore.balance);
+  });
+  
   it('should have an empty inventory', function(){
     assert.equal(0, recordStore.getInventory());
   });
 
   it('should have one record in it\'s inventory', function(){
-    recordStore.addInventory();
-    assert.equal(1, recordStore.getInventory())
+    recordStore.addInventory(recordOne);
+    assert.equal(1, recordStore.getInventory());
   });
+
+  it('should create a function that returns all of a Record\'s details as a well-formatted string');
+  it('should create a function that returns all of a records with a well-formatted string');
+  it('should be able to sell a record and adjust the RecordStore so its balance goes up');
+
+
 
 })
