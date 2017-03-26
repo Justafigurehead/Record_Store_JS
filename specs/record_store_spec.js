61b297658ed6698ea484ed5d47e521803a9ee0d5
Justafigurehead <strachanmichaela@hotmail.com>
2017-03-26 12:12:59 +0100

    added test for finding Store name.

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -3,9 +3,15 @@ var RecordStore = require('../record_store');
 
 describe('Record Store', function(){
 
-  it('should have a Name');
+  beforeEach(function(){
+    recordStore = new RecordStore();
+  })
+
+  it('should have a Name', function(){
+    assert.equal('Big Al\'s Music', recordStore.name);
+  });
   it('should have a City');
   it('should have an empty inventory');
-  it('should have one record in it\s inventory');
+  it('should have one record in it\'s inventory');
 
 })
